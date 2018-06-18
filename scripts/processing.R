9e7de03ee07149c991695314ed0cbf74dafb46d8
Maximilian Rahn <max@Maximilians-MBP.fritz.box>
2018-06-18 23:55:23 +0200

    added menu, filter and categorization rule functionality

--- scripts/processing.R
+++ scripts/processing.R
@@ -2,6 +2,9 @@
 #### parameters ####
 p.import = "import/"
 p.export = "export/"
+p.categories = "categories/categories.csv"
+p.filters = "filters/filters.csv"
+p.cat_rules = "cat_rules/cat_rules.csv"
 
 #### functions ####
 
@@ -45,22 +48,33 @@ preprocess <- function(){
   return(data)
 }
 
-filters.add <- function(column, criterion, value){
+filters.add <- function(column = NULL, criterion = NULL, value = NULL){
+  
+  while(T){
+    if(is.null(column)) column = readline("Enter column >>>")
+    if(is.null(criterion)) criterion = readline("Enter criterion >>>")
+    if(is.null(value)) value = readline("Enter value >>>")
+    
+    cat("confirm new filter rule:\n")
+    cat(paste(column, criterion, value, sep = " "))
+    cat("\nlines which evaluate to true will be removed")
+    if(readline("y/n >>>") == "y"){
+      break
+    } 
+  }
+  
   rule = data.frame(column, criterion, value)
   write.table(rule, 
-            "filters/filters.csv", 
+            p.filters, 
             append = T, 
-            col.names = F, 
-            row.names = F)
+            row.names = F, 
+            col.names = F,
+            sep = ",")
 }
 
 filters.load <- function(n){
-  return(read.csv("filters/filters.csv",
+  return(read.csv(p.filters,
                   stringsAsFactors = F))
-}
-
-filters.remove <- function(column, criterion, value){
-  
 }
 
 #' filtering
@@ -96,17 +110,32 @@ filter <-function(data){
   return(result)
 }
 
-cat_rules.add <- function(column, value, category){
+cat_rules.add <- function(column = NULL, value = NULL, category = NULL){
+  cat("Enter category rule")
+  
+  while(T){
+    if(is.null(column)) column = readline("Enter column >>> ")
+    if(is.null(value)) criterion = readline("Enter value >>> ")
+    if(is.null(category)) value = readline("Enter target category >>> ")
+    
+    cat("confirm new categorization rule:\n")
+    cat(paste(column, value, category, sep = " "))
+    if(readline("y/n >>>") == "y"){
+      break
+    } 
+  }
+  
   df = data.frame(column, value, category)
   write.table(df,
-            "cat_rules/cat_rules.csv",
+            p.cat_rules,
             append = T,
             row.names = F,
-            col.names = F)
+            col.names = F,
+            sep = ",")
 }
 
 cat_rules.load <- function(){
-  cat_rules <- read.csv("cat_rules/cat_rules.csv",
+  cat_rules <- read.csv(p.cat_rules,
                         stringsAsFactors = F)
   return(cat_rules)
 }
@@ -119,10 +148,8 @@ categorize <- function(data){
     result$Kategorie[result[cat_rules[row, "column"]] == cat_rules[row, "value"]] <- cat_rules[row, "category"]
   }
   
-  #result$Kategorie[result$Verwendungszweck == "Haushaltsgeld"] <- "Haushaltsgeld"
   return(result)
 }
-
 
 #' prints provided line to the terminal in structured manner
 #'
@@ -147,7 +174,74 @@ line.print <- function(line, all = F){
   cat(out)
 }
 
-#' asks the user for a classification of the provided line line
+manual.show <- function(){
+  cat("Show valid categories:  1\n")
+  cat("Add category:           2\n")
+  cat("Add filter rule:        3\n")
+  cat("Add category rule:      4\n")
+  
+  while(T){
+  selection <- readline("Enter option: >>>")
+    if(selection %in% c("1")){
+      categories.print()
+      break
+      
+    } else if(selection %in% c("2")){
+      categories.add()
+      break
+      
+    } else if (selection %in% c("3")){
+      filters.add()
+      break
+      
+    } else if (selection %in% c("4")){
+      cat_rules.add()
+      break
+      
+    } else {
+      cat("Selected invalid option")
+    }
+  }
+}
+
+categories.add <- function(category = NULL){
+  
+  if(is.null(category)){
+    while(T){
+      category = readline("Enter new category >>>")
+      
+      res = readline(paste("Confirm: '", category, "' (y/n) >>>", sep = ""))
+      if(res == "y"){
+        break
+      } else if (res == "n"){
+        
+        res = readline("retype? (y/n) >>>")
+        if(res != "y") return()
+        
+      } else cat("Wrong input. Retry!")  
+    
+    }    
+  }
+  
+  df = data.frame(cat = category)
+  write.table(df,
+              p.categories,
+              append = T,
+              row.names = F,
+              col.names = F,
+              sep = ",")
+}
+
+categories.print <- function(){
+  cat("Valid categories:")
+  print(categories.load())
+}
+
+categories.load <- function(){
+  return(read.csv(p.categories))
+}
+
+#' asks the user for a classification of the provided line
 #'
 line.classify <- function(line){
   if(!is.na(line["Kategorie"])) return() 
@@ -155,13 +249,7 @@ line.classify <- function(line){
   cat("Please classify the following line:\n")
   line.print(line)
   
-  valid = c("Essen", 
-            "Freizeit", 
-            "Miete",
-            "Sonstiges",
-            "Foto",
-            "Mobilitaet"
-            )
+  valid = categories.load()
   
   while(T){
     category <- readline("Enter category: ")
@@ -169,8 +257,9 @@ line.classify <- function(line){
     if(category %in% valid){
       return(category)
     } else if (category == "?"){
-      cat("Those are valid categories:\n")
-      cat(valid, sep = "\n")
+      manual.show()
+      valid = categories.load()
+      
     } else{
       cat("You entered an invalid category. Try again or type '?' for help!")
     }
@@ -185,7 +274,6 @@ line.classify <- function(line){
 main <- function(){
   data <- preprocess()
   data <- apply(data, 1, line.classify)
-  
 }
 
 main()
